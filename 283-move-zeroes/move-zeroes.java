1f86d6520da13602f15b7619f75d1f400d818000
joshika086 <joshika086@gmail.com>
2026-08-10 10:32:47 +0530

    Time: 2 ms (92.05%) | Memory: 47.8 MB (38.79%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.length) {
+
+            if (nums[left] != 0) {
+                left++;
+                right++;
+            }
+
+            else if (nums[right] == 0) {
+                right++;
+            }
+
+            else {
+                nums[left] = nums[right];
+                nums[right] = 0;
+
+                left++;
+                right++;
+            }
+        }
+    }
+}
